96dfb5254dbcb4d45178cb1075e5b58f1a9b719a
Anders Aaberg <aaa@nine.dk>
2020-12-29 11:10:20 +0100

    Initial Grails 4.0.6 project

---
+++ grails-app/init/partial/location/uri/servlet/context/path/Application.groovy
@@ -0,0 +1,13 @@
+package partial.location.uri.servlet.context.path
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/partial/location/uri/servlet/context/path/BootStrap.groovy
@@ -0,0 +1,9 @@
+package partial.location.uri.servlet.context.path
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
